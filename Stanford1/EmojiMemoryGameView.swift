23c172256ec480aaa4ca7fdc67be52ccdd6ce2db
abc <abc@abcde-MacBook-Pro.local>
2021-01-18 11:25:13 +0600

    Reactive UI & Layouts

--- Stanford1/EmojiMemoryGameView.swift
+++ Stanford1/EmojiMemoryGameView.swift
@@ -21,7 +21,6 @@ struct EmojiMemoryGameView: View {
         }
         .padding()
         .foregroundColor(.orange)
-        .font(.largeTitle)
     }
 }
 
@@ -29,17 +28,32 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0)
+                RoundedRectangle(cornerRadius: cornerRadius)
                     .fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10.0)
+                RoundedRectangle(cornerRadius: cornerRadius)
                     .fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    //MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
 
